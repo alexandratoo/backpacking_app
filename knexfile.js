7276f2dda9883323221bc0b0967efc4814bf141b
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-04-11 10:15:16 -0600

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
    client: 'pg',
    connection: 'postgres://localhost/backpackingDB'
  },
+ production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
  test: {
    client: 'pg',
    connection: 'postgres://localhost/backpackingDB_test',
