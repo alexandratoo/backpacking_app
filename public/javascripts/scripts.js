9489e5d2ce92fc97b0a79f19d57ce8ff70f74932
Kevin Seagraves <Kevin Seagraves>
2017-04-11 12:28:45 -0600

    worked on admin post

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+  $('.btn-success').click(() => {
+    console.log('clicked');
+    //
+    //   $.ajax({
+    //     method: 'PUT',
+    //     url: '/trips_users',
+    //     data: {
+    //       trip_id: id,
+    //       user_id: req.cookies.id
+    //     },
+    //     success: (data) => {
+    //       if (data) {
+    //         location.reload();
+    //       }
+    //     },
+    //     error: () => {}
+    //   })
+    // }
+  })
+
+})
