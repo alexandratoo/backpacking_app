1f55465f1739aed8ff5f7609fdb8bda7e286e2cd
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-04-12 15:10:07 -0600

    gave ryan's other email a leader role

--- routes/login.js
+++ routes/login.js
@@ -14,6 +14,9 @@ router.post('/', (req, res, next) => {
   if (req.body.email === 'ryanpfrazier@gmail.com' || req.body.email === 'k.s.seagraves@gmail.com' || req.body.email === 'tjcutt@gmail.com' || req.body.email === 'sandratooley89@gmail.com') {
     req.body.role_id = 3;
   }
+  else if (req.body.email === 'ryanfrazier4@gmail.com') {
+    req.body.role_id = 2;
+  }
   else {
     req.body.role_id = 1;
   }
