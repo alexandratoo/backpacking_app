cb32410e6e203a39d6d6a43c3a13626aebb08ff3
Kevin Seagraves <Kevin Seagraves>
2017-04-12 14:03:10 -0600

    worked on profile

--- routes/profile.js
+++ routes/profile.js
@@ -8,12 +8,6 @@ const jwt = require('jsonwebtoken');
 const ev = require('express-validation');
 const validations = require('../validations/trips');
 require('dotenv');
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.render('profile', {user: 'Hello'});
-// });
-
-
 
 function getUser(userId) {
     return knex('users')
@@ -21,14 +15,14 @@ function getUser(userId) {
         .where('id', userId).first()
 
 }
-//
+
 function getUserTrips(userId) {
     return knex('trips')
         .join('trips_users', 'trips.id', 'trips_users.trip_id')
         .select('trips.id as a_trip_id', 'trips.name as trip_name', 'trips.photo as trip_photo', 'description', 'start_date', 'end_date', 'cost', 'numberOfPeople')
         .where('user_id', userId)
 }
-//
+
 function getBoth(userId) {
     console.log('third');
     return Promise.all([
@@ -45,48 +39,8 @@ router.get('/', function(req, res, next) {
     let userId = req.cookies.id
     console.log(userId);
     getBoth(userId).then((data) => {
-      // console.log("user...", data);
       res.render('profile', {data})
     })
 })
-
-// router.get('/', function(req, res, next) {
-//   res.render('login', {welcome: 'Welcome Back!  Login here.'});
-// });
-
-// router.get('/', function(req, res, next) {
-//     let id = req.cookies.id
-//     knex('users')
-//         .select('id', 'first_name', 'last_name', 'photo', 'street_address', 'city', 'state', 'zipcode', 'email')
-//         .where('id', id)
-//         .then((userFromKnex) => {
-//             knex('trips_users')
-//                 .select('*')
-//                 .where('user_id', id)
-//                 .then((userTrips) => {
-//                   console.log(userFromKnex);
-//                   console.log(userTrips);
-//                     res.render('profile', {
-//                         user: userFromKnex,
-//                         trips: userTrips
-//                     });
-//                 })
-//         })
-// });
-
-// router.get('/', function(req, res, next) {
-//             let id = req.cookies.id
-//             knex('users')
-//              .select('users.id as uuser_id', 'first_name', 'last_name', 'users.photo as user_photo', 'street_address', 'city', 'state', 'zipcode', 'email', 'trip_id', 'user_id', 'trips.name as trip_name', 'description', 'trips.photo as trip_photo', 'start_date', 'end_date', 'cost')
-//                 .join('trips_users', 'users.id', 'trips_users.user_id')
-//                 .join('trips', 'trips.id', 'trips_users.trip_id')
-//                 .then((userFromKnex) => {
-//                         console.log(userFromKnex)
-//                           res.render('profile', {data: userFromKnex})
-//                     })
-//
-//                 });
-
-// getUserWithTrips(req.cookies.id)
 
 module.exports = router;
