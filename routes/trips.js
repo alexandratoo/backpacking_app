132ff142e79f2ce38b2613c22e99710bbc4c8656
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-04-12 15:16:59 -0600

    got the res.end() back in trips.js

--- routes/trips.js
+++ routes/trips.js
@@ -57,7 +57,7 @@ router.post('/', (req, res, next) => {
       .returning('*')
       .then(() => {
         return knex.raw('update trips set "numberOfPeople" = ( select count(user_id) from trips_users where trips_users.trip_id = trips.id);')
-        res.end();
+        .then(res.end())
       })
     })
     // .then((theCharge) => {
