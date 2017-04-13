28e410dd3f97ba88edd65264f679aaea294f217c
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-04-13 01:51:53 -0600

    seeding flavor lord

--- seeds/004_trips_users.js
+++ seeds/004_trips_users.js
@@ -14,7 +14,7 @@ exports.seed = function(knex, Promise) {
         {id: 7, trip_id: 1, user_id: 4, stripe_id: 'ajbjsdse', amount: '32400', charge_id: 'hey'},
         {id: 8, trip_id: 2, user_id: 5, stripe_id: 'ajbjdsasdse', amount: '93500', charge_id: 'hey'},
         {id: 9, trip_id: 5, user_id: 6, stripe_id: 'ajbjsdse', amount: '86600', charge_id: 'hey'},
-        {id: 10, trip_id: 4, user_id: 4, stripe_id: 'ajbjsdse', amount: '44500', charge_id: 'hey'},
+        {id: 10, trip_id: 4, user_id: 3, stripe_id: 'ajbjsdse', amount: '44500', charge_id: 'hey'},
         {id: 11, trip_id: 4, user_id: 4, stripe_id: 'ajbjsdse', amount: '44500', charge_id: 'hey'},
         {id: 12, trip_id: 4, user_id: 5, stripe_id: 'ajbjdsasdse', amount: '44500', charge_id: 'hey'},
         {id: 13, trip_id: 4, user_id: 6, stripe_id: 'ajbjsdse', amount: '44500', charge_id: 'hey'}
